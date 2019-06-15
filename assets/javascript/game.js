97c8186102e8c8503f39c6931e45b5d1f2216cae
Anum Asif <unum.asif@gmail.com>
2019-06-14 19:39:11 -0700

    changed html crystal images to buttons with crystal backgrounds. HTML and CSS update.

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//display a random number at the start of the game
+
+//display random number in div #random-number
+
+// var total score = 0;
+
+
+//assign .click to #red and generate random number on click
+//add the number to total score
+
+//assign .click to #blue and generate random number on click
+//add the number to total score
+
+//assign .click to #yellow and generate random number on click
+//add the number to total score
+
+//assign .click to #green and generate random number on click
+//add the number to total score
+
